4a4cbd749b2d0a6879424d78f08120c99c189459
fjodor <wolf.riepl@statistik-dresden.de>
2025-08-12 07:56:22 +0200

    PDF Nachbearbeitung angepasst

--- _pagedown_print-PDF.R
+++ _pagedown_print-PDF.R
@@ -14,16 +14,6 @@ pagedown::chrome_print("Böhme-Dylan-kurz.html",
 
 
 qpdf::pdf_subset(input = "Böhme-Dylan-kurz.pdf",
-           pages = c(1:2, 4:27, 29:71, 74:80, 82, 83),
+           pages = c(1:2, 4:27, 29:72, 75:80, 82, 83),
            output = "Böhme-Dylan-kurz2.pdf")
 
-
-# qpdf::pdf_combine(input = c(
-#   "Reporting_Quarto-dt-it-Trainingsh-1-26.pdf",
-#   "Reporting_Quarto-dt-it-Trainingsh-28-42.pdf",
-#   "libs/_Images/Interaktive-Grafiken_HTML-Widgets.pdf",
-#   "Reporting_Quarto-dt-it-Trainingsh-44-66.pdf"),
-#   output = "Reporting_Quarto-dt-it-Trainingsh-korr.pdf")
-
-# Alte Variante 2024
-# Hier waren es weniger Folien; Versionsverwaltung / github nicht enthalten
